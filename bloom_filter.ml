aa69d5acd77915d2be5444e22ede64d3a3f743d9
Nick Geoca <nickgeoca@gmail.com>
2015-07-28 10:25:10 -0500

    Reorganized code. added either functions

--- bloom_filter.ml
+++ bloom_filter.ml
@@ -1,11 +1,17 @@
+#require "extlib";;
+
+(* -------------------------------------------------- 
+   Util functions *) 
+
 let rec getHashes (obj : 'a) (m : int) (salt : int) (k : int) : int list
-                   = let h = Hashtbl.hash (salt + Hashtbl.hash obj) in           (* hash salt and hashed value. Ensures hash changes every time *)
+                   = let h        = Hashtbl.hash (salt + Hashtbl.hash obj) in  (* hash salt and hashed value. Ensures hash changes every time *)
                      let toMask x = truncate (2.0 ** (float (1 + truncate (log (float x) /. log 2.0))) -. 1.0) in  (* This was tested for correctness *)
-                     let bitmask = toMask m in
-                     let test = bitmask land h
-                     in if k == 0 then []
-                        else if test >= m then  getHashes obj m (salt + 1) k     (* Case where can't use hash, so increase salt *)
-                             else test :: getHashes obj m (salt + 1) (k-1) (* Can use hash, so decrement k *)
+                     let bitmask  = toMask m in
+                     let index    = bitmask land h in
+                     let test     = index < m in           (* index should be less than m... i.e. 'm'=10, so 'index' is 0 to 9 *)
+                     if k == 0 then []
+                     else if test then index :: getHashes obj m (salt + 1) (k-1) (* Can use hash, so decrement k *)
+                          else                  getHashes obj m (salt + 1) k     (* Case where can't use hash, so increase salt *)
 ;;
 
 let getIndexes (obj : 'a) (k : int) (m : int) : int list
@@ -16,24 +22,44 @@ let round (x : float) : int
            = int_of_float (floor (x +. 0.5))
 ;;
 
+(* -------------------------------------------------- 
+   Either type *) 
+
+type ('a, 'b) either = Left of 'a | Right of 'b;;
+
+let getLeft (eith : ('a,'b) either) : 'a list
+      = match eith with
+      | Left x  -> x :: []
+      | Right y -> []
+;;
+
+let getRight (eith : ('a,'b) either) : 'b list
+      = match eith with
+      | Left x  -> []
+      | Right y -> y :: []
+;;
+
+let fmapEither (fn : 'b -> 'c) (eith : ('a,'b) either) : ('a,'c) either
+      = match eith with
+      | Left x  -> Left x
+      | Right y -> Right (fn y)
+;;
+
+
+(* -------------------------------------------------- 
+   Bloom filter *) 
+
 module BloomFilter = 
   struct
     type bloomFilterT = { n    : int;         (* n  Expected number of elements *)
                           p    : float;       (* p  Probability of false positive *) 
                           k    : int;         (* k  Number of hashes *)
                           m    : int;         (* m  Number of bins *) 
-                         bf   : BitSet.t; }   (* Bit array (bloom filter) *)
+                          bf   : BitSet.t; }  (* Bit array (bloom filter) *)
 
-    type ('a, 'b) either = Left of 'a | Right of 'b
-
-    let fmapEither (fn : 'b -> 'c) (eith : ('a,'b) either) : ('a,'c) either
-          = match eith with
-          | Left x  -> Left x
-          | Right y -> Right (fn y)
- 
     let create (n : int) (p : float) : (string,bloomFilterT) either
                 = let m = round (-1.0 *. (float n) *. (log p) /. ((log 2.0) ** 2.0)) in
-                  let k = round ((float m) /. (float n) *. (log 2.0)) in
+                  let k = round ((round m) /. (float n) *. (log 2.0)) in
                   let bFT = { n = n;
                               p = p;
                               m = m;
@@ -61,6 +87,3 @@ module BloomFilter =
                 let result = List.fold_left (&&) true (List.map isSetBf ks) in
                 result
 end
-
-
-
